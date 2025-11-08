1da2f26f7358e633731c8d2ec61474591cf375e8
BOBAvov <v-gashev@list.ru>
2025-11-08 19:10:50 +0300

    Remove .env file from repository

---
+++ internal/logs/slog.go
@@ -0,0 +1,57 @@
+package logs
+
+import (
+	"log/slog"
+	"os"
+
+	"github.com/bmstu-itstech/itsreg-bots/pkg/logs/handlers/slogpretty"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
+)
+
+var defaultLogger *slog.Logger
+
+func DefaultLogger() *slog.Logger {
+	if defaultLogger == nil {
+		env := os.Getenv("APP_ENV")
+		defaultLogger = NewLogger(env)
+	}
+	return defaultLogger
+}
+
+func NewLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case envProd:
+		log = slog.New(
+			slogpretty.PrettyHandlerOptions{
+				SlogOpts: &slog.HandlerOptions{
+					Level: slog.LevelInfo,
+				},
+			}.NewPrettyHandler(os.Stdout),
+		)
+	case envLocal, envDev:
+		log = slog.New(
+			slogpretty.PrettyHandlerOptions{
+				SlogOpts: &slog.HandlerOptions{
+					Level: slog.LevelDebug,
+				},
+			}.NewPrettyHandler(os.Stdout),
+		)
+	default:
+		log = slog.New(
+			slogpretty.PrettyHandlerOptions{
+				SlogOpts: &slog.HandlerOptions{
+					Level: slog.LevelDebug,
+				},
+			}.NewPrettyHandler(os.Stdout),
+		)
+	}
+
+	return log
+}
